bb7f057c0670eaacf3b452a06ba0b33a33ad4fe0
Everton Sávio S. Lucas <everluca@outlook.com>
2020-09-08 20:29:22 -0300

    :sparkles: emmiting comments events

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,15 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({id: commentId, content: content});
 
   commentsByPostId[req.params.id] = comments;
+
+  axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
